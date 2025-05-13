3adecccca2e393464cf30a14c39bb60376d16b79
raghav2219 <abhishekraghav196@gmail.com>
2025-05-13 22:22:22 +0530

    added routes

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as Main } from './main';
+export { default as Product} from './product';
+export { default as Footer } from './footer';
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {Home, Products,AboutPage,ContactPage,Login,Register} from "./pages";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Products />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+  </BrowserRouter>
 );
-
-reportWebVitals();
